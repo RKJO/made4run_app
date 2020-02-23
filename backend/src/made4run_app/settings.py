42985754750b09b7f7c84d6e84383c8e4da24356
rafal <rkjozwiak@gmail.com>
2020-02-23 19:09:55 +0100

    #refactor just comments

--- backend/src/made4run_app/settings.py
+++ backend/src/made4run_app/settings.py
@@ -78,17 +78,30 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'made4run_app.wsgi.application'
 
+## Sqlite3 DB
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+## PostgersQl DB
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'NAME': 'made4run_db',
+        'USER': 'postgres',
+        'PASSWORD': '1987',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -111,6 +124,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+
+# TODO:
+#  know how to translate app
 
 LANGUAGE_CODE = 'pl-pl'
 
