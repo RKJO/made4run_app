61b8eefda259a457d9492028649a3e936184f4b2
rafal <rkjozwiak@gmail.com>
2020-02-23 19:13:20 +0100

    #refactor slug in component Cmpetition

--- frontend/gui/src/components/Competition.js
+++ frontend/gui/src/components/Competition.js
@@ -20,14 +20,9 @@ const Competitions = (props) => {
               pageSize: 3,
             }}
             dataSource={props.data}
-            footer={
-              <div>
-                <b>ant design</b> footer part
-              </div>
-            }
             renderItem={item => (
               <List.Item
-                key={item.name}
+                key={item.title}
                 actions={[
                   <IconText type="star-o" text="156" key="list-vertical-star-o" />,
                   <IconText type="like-o" text="156" key="list-vertical-like-o" />,
@@ -43,7 +38,7 @@ const Competitions = (props) => {
               >
                 <List.Item.Meta
                   avatar={<Avatar src={item.avatar} />}
-                  title={<a href={item.href}>{item.name}</a>}
+                  title={<a href={`/${item.slug}`}>{item.name}</a>}
                   description={item.description}
                 />
                 {item.content}
