ab291e58dcdd3a0afb259a962e5790e779055885
rjozwiak <r.jozwiak@aotm.gov.pl>
2020-02-26 16:54:04 +0100

    create team admin

--- backend/src/teams/admin.py
+++ backend/src/teams/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Team, TeamMembership
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TeamMembership)
+class TeamMembershipAdmin(admin.ModelAdmin):
+    list_display = ("team", "user")
