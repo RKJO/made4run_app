f4845947328ddb8ddb162461c6d70e7b26522fb5
rjozwiak <r.jozwiak@aotm.gov.pl>
2020-02-28 13:25:51 +0100

    add events basic admin

--- backend/src/events/admin.py
+++ backend/src/events/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from events.models import TeamCompetitionEvent, TeamEventParticipants
+
+
+@admin.register(TeamCompetitionEvent)
+class TeamEventAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TeamEventParticipants)
+class TeamEventParticipantsAdmin(admin.ModelAdmin):
+    pass
