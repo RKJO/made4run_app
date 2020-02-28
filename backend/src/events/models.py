1da0ff27d0af235a52f77006d10a51b38308a3df
rjozwiak <r.jozwiak@aotm.gov.pl>
2020-02-28 13:23:53 +0100

    add events models

--- backend/src/events/models.py
+++ backend/src/events/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models.signals import pre_save
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext_lazy as _
+
+from made4run_app import settings
+from competition_calendar.models import UpdateInfoBaseModel, CompetitionModel, DistanceModel
+from teams.models import Team
+
+
+class BaseEvent(UpdateInfoBaseModel):
+    """
+    Base model to automatically set:
+        * create_time
+        * last_update_time
+    and:
+        * create_by
+        * last_update_by
+    Important: "threadlocals middleware" must be used!
+    """
+
+    name = models.CharField(_('Name'), max_length=255, help_text=_("Name of the event"))
+    description = models.TextField(_('description'), help_text=_("Description of the run"), null=True, blank=True)
+    slug = models.SlugField(_('slug'), blank=True, unique=True)
+
+    class Meta:
+        abstract = True
+
+########################################################################################################################
+#                                              User Event
+########################################################################################################################
+
+
+class UserWorkoutEvent(BaseEvent):
+    start_date = models.DateField(_('start date'), help_text=_("Start date of the Workout"))
+    start_time = models.TimeField(_('start time'), auto_now=False, auto_now_add=False,)
+    ascent = models.SmallIntegerField(_('Ascent'), max_length=5, null=True, blank=True)
+    descent = models.SmallIntegerField(_('Descent'), max_length=5, null=True, blank=True)
+    peace = models.SmallIntegerField(_('planned peace'), help_text=_("average pace at which you plan to run."), blank=True, null=True)
+    gpx = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    private = models.BooleanField(_('private'), default=False)
+
+
+class UserWorkoutParticipants(models.Model):
+    user_workout = models.OneToOneField(UserWorkoutEvent, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+########################################################################################################################
+#                                              Team Event
+########################################################################################################################
+
+
+class TeamCompetitionEvent(BaseEvent):
+    competition = models.ForeignKey(CompetitionModel, on_delete=models.CASCADE)
+    team = models.OneToOneField(Team, on_delete=models.CASCADE)
+
+
+class TeamEventParticipants(models.Model):
+    team_event = models.OneToOneField(TeamCompetitionEvent, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    distance = models.ForeignKey(DistanceModel, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'distance')
+
+
+class TeamWorkoutEvent(UserWorkoutEvent):
+    team = models.OneToOneField(Team, on_delete=models.CASCADE)
+
+
+class TeamWorkoutParticipants(models.Model):
+    team_workout = models.OneToOneField(TeamWorkoutEvent, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
