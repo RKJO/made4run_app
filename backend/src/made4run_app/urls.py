20a1524d4143fa7dee93c7c728052470d83b600c
rafal <rkjozwiak@gmail.com>
2020-03-04 16:10:05 +0100

    update main urls

--- backend/src/made4run_app/urls.py
+++ backend/src/made4run_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('competition_calendar.api.urls')),
+    path('api/account', include('account.api.urls')),
+    path('api/competitions', include('competition_calendar.api.urls')),
 ]
