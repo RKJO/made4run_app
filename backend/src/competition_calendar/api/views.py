96f97b6300d150bcdf83ec95d13160444acbb773
rafal <rkjozwiak@gmail.com>
2020-03-05 16:04:56 +0100

    refavtor views

--- backend/src/competition_calendar/api/views.py
+++ backend/src/competition_calendar/api/views.py
@@ -1,17 +1,25 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import viewsets, serializers
+from rest_framework.permissions import AllowAny
+
 from competition_calendar.models import (
     CompetitionModel,
-    DistanceModel,
 )
 
-from .serializers import CompetitionModelSerializer
+from .serializers import (
+    CompetitionModelSerializer,
+)
 
 
-class CompetitionListView(ListAPIView):
-    queryset = CompetitionModel.objects.all()
+class CompetitionViewSet(viewsets.ModelViewSet):
+    """Manage Competition in the database"""
     serializer_class = CompetitionModelSerializer
-
-
-class CompetitionDetailView(RetrieveAPIView):
     queryset = CompetitionModel.objects.all()
-    serializer_class = CompetitionModelSerializer
+    permission_classes = (AllowAny,)
+    lookup_field = 'slug'
+
+    # def get_serializer_class(self):
+    #     """Return appropriate serializer class"""
+    #     if self.action == 'retrieve':
+    #         return serializers.CompetitionModelDetailSerializer
+    #
+    #     return self.serializer_class
