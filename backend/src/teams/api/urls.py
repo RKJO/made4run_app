5cd38b40cb09720baf4a582434cf9baf745510cc
r.jozwiak <r.jozwiak@aotm.gov.pl>
2020-03-13 16:27:03 +0100

    implement drf-nested-routers

--- backend/src/teams/api/urls.py
+++ backend/src/teams/api/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.conf.urls import url
+from rest_framework_nested import routers
 
 from teams.api.views import (
     TeamViewSet,
+    TeamMembershipViewSet,
 )
 
-router = DefaultRouter()
+router = routers.SimpleRouter()
 router.register('', TeamViewSet)
 
-urlpatterns = router.urls
+team_router = routers.NestedSimpleRouter(router, '', lookup='team')
+team_router.register(r'members', TeamMembershipViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(team_router.urls)),
+    ]
