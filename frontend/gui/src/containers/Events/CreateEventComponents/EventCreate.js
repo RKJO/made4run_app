3fee1a498d6fcea8be37e1f436df8d8e9ff7b509
RKJO <rkjozwiak@gmail.com>
2020-06-08 20:42:10 +0200

    rebuild eventCreate

--- frontend/gui/src/containers/Events/CreateEventComponents/EventCreate.js
+++ frontend/gui/src/containers/Events/CreateEventComponents/EventCreate.js
@@ -1,29 +1,14 @@
-// import React, { useState } from "react";
-// import { BaseMap } from "./BaseMap";
+import React, { useState } from "react";
+import { BaseMap } from "./BaseMap";
+import { Sidebar } from "../../../components/Sidebar/Sidebar";
 
-// const EventCreate = () =>  {
-// 	const [markerPosition, setMarkerPosition] = useState({
-// 	  lat: 52.2297,
-// 	  lng: 21.0122
-// 	});
-// 	const { lat, lng } = markerPosition;
+const EventCreate = () => {
+  return (
+    <>
+      <Sidebar />
+      <BaseMap />
+    </>
+  );
+};
 
-// 	const moveMarker = () => {
-// 	  setMarkerPosition({
-// 		lat: lat + 0.0001,
-// 		lng: lng + 0.0001
-// 	  });
-// 	}
-
-// 	return (
-// 	  <div>
-// 		<Map markerPosition={markerPosition} />
-// 		<div>
-// 		  Current markerPosition: lat: {lat}, lng: {lng}
-// 		</div>
-// 		<button onClick={moveMarker}>Move marker</button>
-// 	  </div>
-// 	);
-//   };
-
-// export { EventCreate };
+export { EventCreate };
