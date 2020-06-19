f7941c3ddf4403922b310e911c92b3dc332937d1
RKJO <rkjozwiak@gmail.com>
2020-06-19 15:09:26 +0200

    edit button style

--- frontend/gui/src/containers/Events/EventsPage.js
+++ frontend/gui/src/containers/Events/EventsPage.js
@@ -1,31 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
+import { Switch, Route, useLocation } from "react-router-dom";
+
+import styles from "../../assets/jss/containers/pageTitleComponent";
 
 import { GridContainer } from "../../components/Grid/GridContainer";
 import { GridItem } from "../../components/Grid/GridItem";
 import { Parallax } from "../../components/Parallax/Parallax";
 
-import styles from "../../assets/jss/containers/pageTitleComponent";
+import { EventCreate } from "./CreateEventComponents/EventCreate";
 
-const useStyles = makeStyles(styles);
+const EventPageStyles = {
+  ...styles,
+  main: {
+    ...styles.main,
+    minHeight: "50vh",
+  },
+};
+const useStyles = makeStyles(EventPageStyles);
 
 const EventsPage = (props) => {
   const classes = useStyles();
+  const location = useLocation();
+
+  const showHeaderSection = () => {
+    switch (location.pathname) {
+      case "/events/create_new_event":
+        return false;
+      default:
+        return true;
+    }
+  };
 
   return (
-    <Parallax small filter image={require("../../assets/img/events.jpg")}>
-      <div className={classes.container}>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={6}>
-            <h1 className={classes.title}>
-              Wydarzenia <span className={classes.danger}></span>
-            </h1>
-            <h4>Palnuj treningi, zawody, poznawiaj nowch ludzi.</h4>
-          </GridItem>
-        </GridContainer>
-      </div>
-    </Parallax>
+    <>
+      {showHeaderSection() && (
+        <Parallax small filter image={require("../../assets/img/events.jpg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={6}>
+                <h1 className={classes.title}>
+                  Wydarzenia <span className={classes.danger}></span>
+                </h1>
+                <h4>Palnuj treningi, zawody, poznawiaj nowch ludzi.</h4>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+      )}
+      <main className={classes.main}>
+        <Switch>
+          <Route
+            exect
+            path='/events/create_new_event'
+            component={EventCreate}
+          />
+          <Route exect path='/events' component={EventCreate} />
+        </Switch>
+      </main>
+    </>
   );
 };
 
