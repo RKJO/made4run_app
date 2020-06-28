81b456c001e8600831440a02088e550542a677af
RKJO <rkjozwiak@gmail.com>
2020-06-28 20:33:54 +0200

    changr Browser router to hashrouter as Router

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import { BaseLayout } from "./containers/BaseLayout";
 import { LandingPage } from "./containers/LandingPage/LandingPage";
@@ -10,24 +10,24 @@ import { EventsPage } from "./containers/Events/EventsPage";
 import { Credits } from "./containers/Credits/Credtis";
 
 const App = (props) => {
-	return (
-		<BrowserRouter>
-			<BaseLayout>
-				<Switch>
-					{/* <Route
+  return (
+    <Router>
+      <BaseLayout>
+        <Switch>
+          {/* <Route
             exect
             path='/competitions/:competitionSlug'
             component={CompetitionDetail}
           /> */}
-					<Route path='/events*' component={EventsPage} />
-					<Route path='/teams*' component={TeamsPage} />
-					<Route path='/competitions*' component={CompetitionsPage} />
-					<Route path='/credits' component={Credits} />
-					<Route exect path='/' component={LandingPage} />
-				</Switch>
-			</BaseLayout>
-		</BrowserRouter>
-	);
+          <Route path='/events*' component={EventsPage} />
+          <Route path='/teams*' component={TeamsPage} />
+          <Route path='/competitions*' component={CompetitionsPage} />
+          <Route path='/credits' component={Credits} />
+          <Route exect path='/' component={LandingPage} />
+        </Switch>
+      </BaseLayout>
+    </Router>
+  );
 };
 
 export default App;
