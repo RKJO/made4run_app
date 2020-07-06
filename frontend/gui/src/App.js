9963b20e2395a2234fc70e67a13807e09f191f20
RKJO <rkjozwiak@gmail.com>
2020-07-06 20:05:18 +0200

    import Context

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
+import { AuthState } from "./context/auth/AuthState";
 import { BaseLayout } from "./containers/BaseLayout";
 import { LandingPage } from "./containers/LandingPage/LandingPage";
 import { CompetitionsPage } from "./containers/Competitions/CompetitionsPage";
@@ -8,27 +9,32 @@ import { TeamsPage } from "./containers/Teams/TeamsPage";
 import { EventsPage } from "./containers/Events/EventsPage";
 
 import { Credits } from "./containers/Credits/Credtis";
-import { LogiInPage } from "./containers/Registration/LogiInPage";
+
+import { LoginPage } from "./containers/Registration/LoginPage";
+import { RegisterPage } from "./containers/Registration/RegisterPage";
 
 const App = (props) => {
   return (
-    <Router>
-      <BaseLayout>
-        <Switch>
-          {/* <Route
+    <AuthState>
+      <Router>
+        <BaseLayout>
+          <Switch>
+            {/* <Route
             exect
             path='/competitions/:competitionSlug'
             component={CompetitionDetail}
           /> */}
-          <Route path='/events*' component={EventsPage} />
-          <Route path='/teams*' component={TeamsPage} />
-          <Route path='/competitions*' component={CompetitionsPage} />
-          <Route path='/credits' component={Credits} />
-          <Route path='/login' component={LogiInPage} />
-          <Route exect path='/' component={LandingPage} />
-        </Switch>
-      </BaseLayout>
-    </Router>
+            <Route path='/events*' component={EventsPage} />
+            <Route path='/teams*' component={TeamsPage} />
+            <Route path='/competitions*' component={CompetitionsPage} />
+            <Route path='/credits' component={Credits} />
+            <Route path='/register' component={RegisterPage} />
+            <Route path='/login' component={LoginPage} />
+            <Route exect path='/' component={LandingPage} />
+          </Switch>
+        </BaseLayout>
+      </Router>
+    </AuthState>
   );
 };
 
