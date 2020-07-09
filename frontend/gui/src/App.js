e0890c0110841012596682dc86f30addd89fdf02
RKJO <rkjozwiak@gmail.com>
2020-07-09 15:58:09 +0200

    implement alert state

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import { AuthState } from "./context/auth/AuthState";
+import { AlertState } from "./context/alert/AlertState";
+
 import { BaseLayout } from "./containers/BaseLayout";
 import { LandingPage } from "./containers/LandingPage/LandingPage";
 import { CompetitionsPage } from "./containers/Competitions/CompetitionsPage";
@@ -10,30 +12,30 @@ import { EventsPage } from "./containers/Events/EventsPage";
 
 import { Credits } from "./containers/Credits/Credtis";
 
-import { LoginPage } from "./containers/Registration/LoginPage";
-import { RegisterPage } from "./containers/Registration/RegisterPage";
+import { RegistrationPage } from "./containers/Registration/RegistrationPage";
 
 const App = (props) => {
   return (
     <AuthState>
-      <Router>
-        <BaseLayout>
-          <Switch>
-            {/* <Route
+      <AlertState>
+        <Router>
+          <BaseLayout>
+            <Switch>
+              {/* <Route
             exect
             path='/competitions/:competitionSlug'
             component={CompetitionDetail}
           /> */}
-            <Route path='/events*' component={EventsPage} />
-            <Route path='/teams*' component={TeamsPage} />
-            <Route path='/competitions*' component={CompetitionsPage} />
-            <Route path='/credits' component={Credits} />
-            <Route path='/register' component={RegisterPage} />
-            <Route path='/login' component={LoginPage} />
-            <Route exect path='/' component={LandingPage} />
-          </Switch>
-        </BaseLayout>
-      </Router>
+              <Route path='/events*' component={EventsPage} />
+              <Route path='/teams*' component={TeamsPage} />
+              <Route path='/competitions*' component={CompetitionsPage} />
+              <Route path='/credits' component={Credits} />
+              <Route path='/registration*' component={RegistrationPage} />
+              <Route exect path='/' component={LandingPage} />
+            </Switch>
+          </BaseLayout>
+        </Router>
+      </AlertState>
     </AuthState>
   );
 };
